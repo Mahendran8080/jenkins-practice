78e2fcb641f773b6127c4242ac817c4329ab4775
mahendran8080 <mahendrancool18@gmail.com>
2025-12-26 08:01:59 +0000

    Testing poll SCM

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Jenkins CI Pipeline 🚀');
+  res.send('Hello from Jenkins CI Pipeline with poll SCM');
 });
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
